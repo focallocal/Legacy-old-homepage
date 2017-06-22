faee9a9f5fd7afa8cb9b852ac8813d68ab9f796a
AndyatFocallocal <focallocal.london@gmail.com>
2017-06-23 00:03:20 +0100

    updated text. added new container below icons

--- imports/core/ui/components/About.jsx
+++ imports/core/ui/components/About.jsx
@@ -7,22 +7,31 @@ export class About extends PureComponent {
         <div className="container">
           <h2>What is Focallocal?</h2>
           <p>
-            <span>Focallocal is a way of living - and also an open, welcoming community made up of thousands of positive people, together targeting the root causes of any, and all societal issues<br></span>
-            <span>Since 2011 our Movement has been exploring new ideas, and fun activities which empower people, all around the world, to build a happier future for everyone.</span>
-            <span>Join in and turn Positive Action into a daily habit to explore the power you have to change the world, and the happiness it will bring into your life.</span>
+            <span>Focallocal is an open, welcoming community of thousands of positive people around the world</span>
+          </p>
+          <p>
+            <span>Since 2011 we have been coming together and targeting the root causes of any, and all societal issues</span>
+          </p>
+          <p>
+            <span>Sharing ideas, and using fun activities to empower people to build a friendlier, happier more connected future for everyone</span>
           </p>
           <div className="items">
             <div className="item">
               <div className="icon smiley" />
-              <h4>Make the world better every day</h4>
+              <h4>make the world ‘happier’ every day</h4>
+            </div>
+            <div className="container">
+              <p>
+                <span>Join us and turn Positive Action into a daily habit to explore your power to change the world, and the happiness it'll bring into your life</span>
+              </p>
             </div>
             <div className="item">
               <div className="icon friends" />
-              <h4>Make the world better every day</h4>
+              <h4>make ‘positive’ friends all over the world</h4>
             </div>
             <div className="item">
               <div className="icon share" />
-              <h4>Make the world better every day</h4>
+              <h4>share/learn skills to target local issues</h4>
             </div>
           </div>
         </div>
