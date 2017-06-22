66412d41b36de0a3aecbf1c3f7a95a57a77173e4
AndyatFocallocal <focallocal.london@gmail.com>
2017-06-22 23:31:21 +0100

    <br> ?

--- imports/core/ui/components/About.jsx
+++ imports/core/ui/components/About.jsx
@@ -7,7 +7,7 @@ export class About extends PureComponent {
         <div className="container">
           <h2>What is Focallocal?</h2>
           <p>
-            <span>Focallocal is a way of living - and also an open, welcoming community made up of thousands of positive people, together targeting the root causes of any, and all societal issues.</span>
+            <span>Focallocal is a way of living - and also an open, welcoming community made up of thousands of positive people, together targeting the root causes of any, and all societal issues<br></span>
             <span>Since 2011 our Movement has been exploring new ideas, and fun activities which empower people, all around the world, to build a happier future for everyone.</span>
             <span>Join in and turn Positive Action into a daily habit to explore the power you have to change the world, and the happiness it will bring into your life.</span>
           </p>
