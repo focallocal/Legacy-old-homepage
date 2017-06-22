24e784e09679c7e12e8404977339a5a3b5a9da38
AndyatFocallocal <focallocal.london@gmail.com>
2017-06-23 00:16:11 +0100

    removed capital and full stop

--- imports/core/ui/components/Adventures.jsx
+++ imports/core/ui/components/Adventures.jsx
@@ -11,7 +11,8 @@ export class Adventures extends PureComponent {
               <iframe width="100%" height="315" src="https://www.youtube.com/embed/WUppGn2Z4b4?list=PLzwMXFvS_OYMIrrdyP1ASjmD3uz9NqC9p" frameBorder="0" allowFullScreen />
             </div>
             <div className="text-cont">
-              <p>You'll be a part of a growing global movement with a plan to change the world. Meeting positive, pro-active, awesome new friends both locally, and globally. Together using simple, fun activities and exploring new ideas to shape friendlier, happier communities everywhere and bring more peace in our World. </p>
+              <p>You'll be a part of a growing global movement with a plan to change the world. Meeting positive, pro-active, awesome new friends both locally, and globally. Together using simple, fun activities and exploring new ideas to shape friendlier, happier communities everywhere and bring more peace in our world
+              </p>
             </div>
           </div>
         </div>
