cd0479df9f1a67299405f61e888bfddd1f5acec4
AndyatFocallocal <focallocal.london@gmail.com>
2017-06-23 00:18:36 +0100

    switched other video

--- imports/core/ui/components/Adventures.jsx
+++ imports/core/ui/components/Adventures.jsx
@@ -8,7 +8,7 @@ export class Adventures extends PureComponent {
           <h2>What adventures await me?</h2>
           <div className="row">
             <div>
-              <iframe width="100%" height="315" src="https://www.youtube.com/embed/WUppGn2Z4b4?list=PLzwMXFvS_OYMIrrdyP1ASjmD3uz9NqC9p" frameBorder="0" allowFullScreen />
+              <iframe width="100%" height="315" src="https://www.youtube.com/embed/tG4l514O0t4" frameBorder="0" allowFullScreen />
             </div>
             <div className="text-cont">
               <p>You'll be a part of a growing global movement with a plan to change the world. Meeting positive, pro-active, awesome new friends both locally, and globally. Together using simple, fun activities and exploring new ideas to shape friendlier, happier communities everywhere and bring more peace in our world
