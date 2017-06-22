bd3b445c362f617f58a3224703730fbc7ab65516
AndyatFocallocal <focallocal.london@gmail.com>
2017-06-23 00:17:57 +0100

    switched videos

--- imports/core/ui/components/Mission.jsx
+++ imports/core/ui/components/Mission.jsx
@@ -8,7 +8,7 @@ export class Mission extends PureComponent {
           <h2>What is our mission and message?</h2>
           <div className="row">
             <div>
-              <iframe width="100%" height="315" src="https://www.youtube.com/embed/tG4l514O0t4" frameBorder="0" allowFullScreen />
+              <iframe width="100%" height="315" src="https://www.youtube.com/embed/WUppGn2Z4b4?list=PLzwMXFvS_OYMIrrdyP1ASjmD3uz9NqC9p" frameBorder="0" allowFullScreen />
             </div>
             <div className="text-cont">
               <p>It’s time to stop waiting and realise that we all have the power to solve ALL of society's problems ourselves by coming together and using positive and innovative tools that target the roots causes of those issues. Each action may only feel like a tiny ripple, but together as a movement all around the world these ripples combine, growing into an unstoppable wave of positive change! Focallocal is about empowering people, because together we can make a happier and safer future for everyone. The movement has already begun, and being a part of it is easy and a whole lot of fun! </p>
