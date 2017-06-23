05fd7ad716a9e29d7f9b5370e42b0e65c4024cd3
AndyatFocallocal <focallocal.london@gmail.com>
2017-06-23 01:24:08 +0100

    updated text

--- imports/core/ui/components/Mission.jsx
+++ imports/core/ui/components/Mission.jsx
@@ -11,7 +11,7 @@ export class Mission extends PureComponent {
               <iframe width="100%" height="315" src="https://www.youtube.com/embed/WUppGn2Z4b4?list=PLzwMXFvS_OYMIrrdyP1ASjmD3uz9NqC9p" frameBorder="0" allowFullScreen />
             </div>
             <div className="text-cont">
-              <p>Times are changing, and so can our feeling of powerless at the pain we see every day around us, and on the news<br />Today all have the power to solve ALL of the world's problems ourselves by coming together and using positive and innovative tools to target their roots causes<br />While each action may feel like a tiny ripple. As the Positve Action Movement sweeps around the world these ripples, will combine into an unstoppable wave of positive change!<br />Focallocal is about empowering people<br />The time for waiting for others to fix this planet is over. That future is ours, if we all just do a little, every day!</p>
+              <p>Times are changing, and so can our feeling of powerless at the pain we see every day around us, and on the news<br />Today's technology gives us ALL have the power to solve ALL of the world's problems ourselves, by coming together and using positive and innovative tools to target their roots causes<br />While each action may feel like a tiny ripple. As the Positve Action Movement sweeps around the world these ripples will combine into an unstoppable wave of positive change!<br />Focallocal is about empowering people. The time for waiting for others to fix this planet is over<br />That future is ours, if we all just do a little Positive Action, every day!</p>
             </div>
           </div>
         </div>
